c3b99d126ec02e2777a5107b1623c6bda5d036d9
Ruslan <ruslan.vafin.93@gmail.com>
2021-10-14 18:50:27 +0300

    add check for links with offer

--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,15 @@
 from .pages.product_page import ProductPage
 import time
+import pytest
 
 
-def test_guest_can_add_product_to_basket(driver):
-    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+@pytest.mark.parametrize('offer', ['0', '1', '2', '3', '4', '5', '6',
+                                   pytest.param('7', marks=pytest.mark.xfail), '8', '9'])
+def test_guest_can_add_product_to_basket(driver, offer):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer}'
     page = ProductPage(driver, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-    time.sleep(2)
     page.should_be_equal_product_name()
     page.should_be_equal_product_price()
